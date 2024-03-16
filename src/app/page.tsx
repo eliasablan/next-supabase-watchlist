6c06d497424ed7d99b8ec580b28c2b6a6d92a474
Elias Ablan <eliasablan93@gmail.com>
2024-03-15 22:16:01 -0400

    Add sign in with google provider to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
+import AuthForm from "@/components/AuthForm";
+
 export default async function Home() {
   const supabase = createClient();
   const {
@@ -11,5 +13,22 @@ export default async function Home() {
     redirect("/watch-list");
   }
 
-  return <main>home</main>;
+  return (
+    <main className="min-h-screen">
+      <div className="container mx-auto p-6 sm:p-12">
+        <h1 className="text-4xl md:text-5xl font-extrabold mb-6">
+          Welcome to Watch List
+        </h1>
+        <p className="text-lg md:text-xl mb-6">
+          Your personal space to curate and manage a wishlist of your favorite
+          watches. Sign in to create, view, edit and delete items from your
+          watchlist.
+        </p>
+        <div className="p-6 rounded-lg shadow-lg">
+          <h2 className="text-2xl font-semibold">Sign in</h2>
+          <AuthForm />
+        </div>
+      </div>
+    </main>
+  );
 }
---
+++ src/components/AuthForm.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { Auth } from "@supabase/auth-ui-react";
+import { createClient } from "@/utils/supabase/client";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { useState, useEffect } from "react";
+
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    "http://localhost:3000/";
+  // Include `https://` when not localhost.
+  url = url.includes("http") ? url : `https://${url}`;
+  // Include a trailing `/`.
+  url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+  return url;
+};
+
+export default function AuthForm() {
+  const supabase = createClient();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return (
+      <div className="animate-pulse flex space-x-4 rounded-lg border my-3 py-2 justify-center items-center">
+        <div className="rounded-full bg-slate-200 h-6 w-6"></div>
+        <div className="h-3 w-44 bg-slate-200 rounded"></div>
+      </div>
+    );
+  }
+
+  return (
+    <Auth
+      supabaseClient={supabase}
+      view="sign_in"
+      providers={["google"]}
+      onlyThirdPartyProviders
+      redirectTo={getURL() + "auth/callback"}
+      appearance={{
+        theme: ThemeSupa,
+        variables: {
+          default: {
+            colors: {
+              brand: "red",
+              brandAccent: "darkred",
+            },
+          },
+        },
+      }}
+    />
+  );
+}
