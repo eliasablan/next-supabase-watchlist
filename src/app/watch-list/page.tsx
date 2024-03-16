6cdb395c82d11ee9b4f88d11bb15a1bae3800cd9
Elias Ablan <eliasablan93@gmail.com>
2024-03-16 15:50:23 -0400

    Add sign out to logged user in watchlist page

--- src/app/watch-list/page.tsx
+++ src/app/watch-list/page.tsx
@@ -11,7 +11,25 @@ const watchlist = async () => {
     redirect("/");
   }
 
-  return <div>watchlist</div>;
+  return (
+    <div className="min-h-screen">
+      <div className="container mx-auto p-6 sm:p-12">
+        <div className="flex justify-between items-start">
+          <h1 className="text-4xl md:text-5xl font-extrabold mb-6">
+            My Watch List
+          </h1>
+          <form action="/auth/signout" method="post">
+            <button
+              type="submit"
+              className="bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Sign out
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default watchlist;
