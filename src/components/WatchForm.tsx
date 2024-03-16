2017b6be5f7d34ce5ec01cbc1f2744112429bb1e
Elias Ablan <eliasablan93@gmail.com>
2024-03-16 16:31:53 -0400

    WatchForm component created

---
+++ src/components/WatchForm.tsx
@@ -0,0 +1,49 @@
+import addWatch from "@/server-actions/addWatch";
+
+export default function WatchForm() {
+  return (
+    <form action={addWatch} className="mb-6">
+      <div className="mb-4">
+        <label htmlFor="brand" className="block mb-2">
+          Brand
+        </label>
+        <input
+          type="text"
+          id="brand"
+          name="brand"
+          className="shadow appearance-none bg-gray-700 text-white rounded w-full py-2 px-3"
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="model" className="block mb-2">
+          Model
+        </label>
+        <input
+          type="text"
+          id="model"
+          name="model"
+          className="shadow appearance-none bg-gray-700 text-white rounded w-full py-2 px-3"
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="referenceNumber" className="block mb-2">
+          Reference Number
+        </label>
+        <input
+          type="text"
+          id="referenceNumber"
+          name="referenceNumber"
+          className="shadow appearance-none bg-gray-700 text-white rounded w-full py-2 px-3"
+        />
+      </div>
+      <button
+        type="submit"
+        className="bg-gray-600 hover:bg-gray-300 text-white hover:text-black font-bold py-2 px-4 rounded"
+      >
+        Add Watch
+      </button>
+    </form>
+  );
+}
